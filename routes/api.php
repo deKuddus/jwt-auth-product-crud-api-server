c91fe60e7de9a39209c3ddcca7c802f4cbe9cd1c
deKuddus <ma.kuddus37@gmail.com>
2020-12-16 15:05:53 -0500

    authentication part done

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        // Below mention routes are public, user can access those without any restriction.
+        // Create New User
+        Route::post('register', 'AuthenticationController@register');
+        // Login User
+        Route::post('login', 'AuthenticationController@login');
+
+        // Refresh the JWT Token
+        Route::get('refresh', 'AuthenticationController@refresh');
+
+        // Below mention routes are available only for the authenticated users.
+        Route::middleware('auth:api')->group(function () {
+            // Get user info
+            Route::get('user', 'AuthenticationController@user');
+            Route::get('token_user', 'AuthenticationController@request_user');
+            // Logout user from application
+            Route::post('logout', 'AuthenticationController@logout');
+        });
+    });
+});
